2ae086ab8465008afd766beb20d2cf2d44b1f868
sujoyDU <sujoydebnath.92@gmail.com>
2019-12-08 14:35:42 -0500

    problem 1

---
+++ TwoSum.py
@@ -0,0 +1,24 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+"""
+
+#Here the idea is to create a dictionary of items with their index numbers from the list
+# whose values does not compute the target. 
+# when target -val aka the complement  is found in the list then thats the desired index
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        itemDict = {}
+        for idx, val in enumerate(nums):
+            if (target - val) not in itemDict:
+                itemDict[val] = idx
+            else: return [itemDict[target-val],idx]
